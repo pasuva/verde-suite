acf85202ab958315a3ea3fe77e906462ddb5b78a
El Juancho <70743149+El-Juancho@users.noreply.github.com>
2026-04-07 15:06:10 +0200

    a tomar por culo

--- PROYECTO_SYMTEL_POSTGRESQL_V1/codigo_limpio/modules/__init__.py
+++
@@ -1,2 +0,0 @@
-# __init__.py en modules (puede estar vacío o incluir configuraciones)
-from . import login, admin, comercial_jefe, comercial_rafa, comercial_vip, marketing, rol_viabilidad, auditor
--- PROYECTO_SYMTEL_POSTGRESQL_V1/codigo_limpio/modules/cookie_instance.py
+++
@@ -1,5 +0,0 @@
-# cookie_instance.py
-from streamlit_cookies_controller import CookieController
-
-# Instanciamos la cookie controller con la key "cookies"
-controller = CookieController(key='cookies')
